570bb6c1344b6482cf1faf6e012bcc26d1d1563f
hpctraining56 GKRS <hpctraining56@gwdu102.global.gwdg.cluster>
2023-07-01 22:18:23 +0200

    Removing Required packages in CMakeList

---
+++ build/CMakeFiles/HPC.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HPC.dir/main.c.o"
+  "CMakeFiles/HPC.dir/main.c.o.d"
+  "HPC"
+  "HPC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HPC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
